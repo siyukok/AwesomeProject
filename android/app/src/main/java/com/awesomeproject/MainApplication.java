135ac08705d9051d3e1042c7e4ee8c74c90e08b2
siyukok <devilkeven@outlook.com>
2017-12-04 16:41:21 +0800

    Android Module

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -1,10 +1,20 @@
 package com.awesomeproject;
 
 import android.app.Application;
+import android.support.annotation.Nullable;
 
+import com.awesomeproject.react.MyReactPackage;
+import com.awesomeproject.react.RNBridge;
+import com.awesomeproject.react.ReadingNativeModuleCallExceptionHandler;
+import com.facebook.infer.annotation.Assertions;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactInstanceManagerBuilder;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.common.LifecycleState;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
@@ -12,8 +22,41 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  public static MainApplication application;
+  public static ReactApplicationContext reactContext;
+
+  //给JS发送消息
+  public static void sendEvent(String eventName,@Nullable WritableMap params){
+    RNBridge rnBridge = new RNBridge(reactContext);
+    rnBridge.sendEvent(eventName, params);
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected ReactInstanceManager createReactInstanceManager() {
+      ReactInstanceManagerBuilder builder = ReactInstanceManager.builder()
+              .setApplication(getApplication())
+              .setJSMainModulePath(getJSMainModuleName())
+              .setUseDeveloperSupport(getUseDeveloperSupport())
+              .setRedBoxHandler(getRedBoxHandler())
+              .setUIImplementationProvider(getUIImplementationProvider())
+              .setInitialLifecycleState(LifecycleState.BEFORE_CREATE)
+              .setNativeModuleCallExceptionHandler(
+                      new ReadingNativeModuleCallExceptionHandler());
+
+      for (ReactPackage reactPackage : getPackages()) {
+        builder.addPackage(reactPackage);
+      }
+
+      String jsBundleFile = getJSBundleFile();
+      if (jsBundleFile != null) {
+        builder.setJSBundleFile(jsBundleFile);
+      } else {
+        builder.setBundleAssetName(Assertions.assertNotNull(getBundleAssetName()));
+      }
+      return builder.build();
+    }
+
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -41,6 +84,8 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    application = this;
+    reactContext = new ReactApplicationContext(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
